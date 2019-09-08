b73b1cd529b035d0de54a1e461bd96c25be91d0e
Mike <mike.huang.mikank@gmail.com>
2019-09-08 15:41:02 +0800

    Update link path

--- src/components/AdminNav.vue
+++ src/components/AdminNav.vue
@@ -2,7 +2,7 @@
   <div class="mb-3">
     <h1>餐廳後台</h1>
     <router-link to="/admin/restaurants">Restaurant</router-link>&nbsp; |
-    <a href="#">Categories</a> |
+    <router-link to="/admin/categories">Categories</router-link>&nbsp; |
     <router-link to="/admin/users">Users</router-link>
   </div>
 </template>
