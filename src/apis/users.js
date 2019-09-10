3a9cc66d5291537ec5ac3e364b7ba2a16be10918
Mike <mike.huang.mikank@gmail.com>
2019-09-10 15:57:28 +0800

    Integrate API with top users page follow and unfollow feature

--- src/apis/users.js
+++ src/apis/users.js
@@ -27,5 +27,15 @@ export default {
     return apiHelper.get('/users/top', {
       headers: { Authorization: `Bearer ${getToken()}` }
     })
+  },
+  addFollowing({ userId }) {
+    return apiHelper.post(`/following/${userId}`, null, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  removeFollowing({ userId }) {
+    return apiHelper.delete(`/following/${userId}`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
   }
 }
--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -6,7 +6,28 @@
 
     <div class="row text-center">
       <!--User Card-->
-      <UserCard v-for="user in users" :key="user.id" :initial-user="user" />
+      <!-- <UserCard v-for="user in users" :key="user.id" :initial-user="user" /> -->
+      <div v-for="user in users" :key="user.id" class="col-6 col-md-3">
+        <a href="#">
+          <img :src="user.image" width="140px" height="140px" />
+        </a>
+        <h2>{{user.name}}</h2>
+        <span class="badge badge-secondary">追蹤人數：{{user.FollowerCount}}</span>
+        <p class="mt-3">
+          <button
+            v-if="user.isFollowed"
+            @click.stop.prevent="unfollow(user.id)"
+            type="button"
+            class="btn btn-danger"
+          >取消追蹤</button>
+          <button
+            v-else
+            @click.stop.prevent="follow(user.id)"
+            type="button"
+            class="btn btn-primary"
+          >追蹤</button>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -15,7 +36,6 @@
 import userAPI from "../apis/users";
 import { Toast } from "../utils/helpers";
 import NavTabs from "../components/NavTabs";
-import UserCard from "../components/UserCard";
 
 export default {
   data() {
@@ -24,8 +44,7 @@ export default {
     };
   },
   components: {
-    NavTabs,
-    UserCard
+    NavTabs
   },
   created() {
     this.fetchUser();
@@ -44,6 +63,60 @@ export default {
         Toast.fire({
           type: "error",
           title: "Cannot get top users, please try again later"
+        });
+      }
+    },
+    async follow(userId) {
+      try {
+        const { data, statusText } = await userAPI.addFollowing({ userId });
+        // error handling
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+        // update user data
+        this.users = this.users
+          .map(user => {
+            if (user.id !== userId) {
+              return user;
+            }
+            return {
+              ...user,
+              isFollowed: true,
+              FollowerCount: (user.FollowerCount += 1)
+            };
+          })
+          .sort((a, b) => b.FollowerCount - a.FollowerCount);
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "Cannot follow this user, please try again later"
+        });
+      }
+    },
+    async unfollow(userId) {
+      try {
+        const { data, statusText } = await userAPI.removeFollowing({ userId });
+        // error handling
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+        //update user data
+        this.users = this.users
+          .map(user => {
+            if (user.id !== userId) {
+              return user;
+            }
+            return {
+              ...user,
+              isFollowed: false,
+              FollowerCount: (user.FollowerCount -= 1)
+            };
+          })
+          .sort((a, b) => b.FollowerCount - a.FollowerCount);
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "Cannnot unfollow this user, please try again later"
         });
       }
     }
